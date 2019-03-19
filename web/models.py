f96bf4c4d963cb1c04343d15e613272afd44019f
Edward <edwardlshfzw@gmail.com>
2019-03-19 11:28:30 +0800

    完成用户管理的基本操作

--- web/models.py
+++ web/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rbac.models import UserInfo as RbacUserInfo
 
 
 class School(models.Model):
@@ -19,3 +20,24 @@ class Department(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class UserInfo(RbacUserInfo):
+    """
+    员工表
+    """
+    MALE = 1
+    FEMALE = 2
+    GENDER_ITEMS = (
+        (MALE, '男'),
+        (FEMALE, '女'),
+    )
+    realname = models.CharField(verbose_name='真实姓名', max_length=16)
+    phone = models.CharField(verbose_name='手机号', max_length=32)
+    gender = models.PositiveIntegerField(verbose_name='性别', choices=GENDER_ITEMS, default=MALE)
+    department = models.ForeignKey(to=Department, verbose_name='部门', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
--- web/stark.py
+++ web/stark.py
@@ -1,4 +1,4 @@
-from stark.service.version1 import site, StarkHandler
+from stark.service.version1 import site, StarkHandler, get_choice_text, StarkModelForm
 from web import models
 
 
@@ -10,5 +10,17 @@ class DepartmentHandler(StarkHandler):
     list_display = ['title']
 
 
+class UserInfoModelForm(StarkModelForm):
+    class Meta:
+        model = models.UserInfo
+        fields = ['name', 'password', 'realname', 'gender', 'phone', 'email', 'department', 'roles']
+
+
+class UserInfoHandler(StarkHandler):
+    list_display = ['realname', get_choice_text('性别', 'gender'), 'phone', 'email', 'department']
+    model_form_class = UserInfoModelForm
+
+
 site.register(models.School, SchoolHandler)
 site.register(models.Department, DepartmentHandler)
+site.register(models.UserInfo, UserInfoHandler)
